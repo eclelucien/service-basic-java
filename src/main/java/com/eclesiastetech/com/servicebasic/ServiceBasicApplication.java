e58092f35902f0ef65eabaa324e70215e9fe86eb
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-05-09 22:29:27 -0300

    initial commit

---
+++ src/main/java/com/eclesiastetech/com/servicebasic/ServiceBasicApplication.java
@@ -0,0 +1,13 @@
+package com.eclesiastetech.com.servicebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eclesiastetech/com/servicebasic/ServiceBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclesiastetech.com.servicebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
